8d5675fa32d9d660db7c41df4e565bb31c310307
Iamneo <techadmin@iamneo.ai>
2023-09-03 18:22:04 +0000

    Backup Commit

--- jfs/script.js
+++ jfs/script.js
@@ -1,2 +1,8 @@
 const selectBox=document.querySelector(".select-box"),
-selectBtnX=selectBox.querySelector("")
+selectBtnX=selectBox.querySelector(".options.playerX"),
+selectBtnO=selectBox.querySelector(".options.playerO"),
+playBoard=document.querySelector(".play-board"),
+players=document.querySelector(".players"),
+allBox=document.querySelectorAll("section span"),
+resultBox=document.querySelector(".result-box"),
+wonText=resultbo
